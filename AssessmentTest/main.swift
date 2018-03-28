a50d9baaae06318e2dc79091011b598b493301bf
Chanchal Raj <rajchanchalkhatri@gmail.com>
2018-03-28 06:09:22 +0400

    Initial Commit

---
+++ AssessmentTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AssessmentTest
+//
+//  Created by Chanchal Raj on 28/03/2018.
+//  Copyright © 2018 Khatri Corporation. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
